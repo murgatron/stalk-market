3697e90a3cf6966e33b5284a57e802d2361bd1bb
murgatron <megan.e.meyer@protonmail.com>
2021-02-17 08:29:51 -0700

    data entry

--- server/Migrations/20210214_InitialMigration.cs
+++ server/Migrations/20210214_InitialMigration.cs
@@ -3,30 +3,31 @@ using FluentMigrator;
 
 namespace Migrations
 {
-    [Migration(20210214121800)]
-    public class InitialMigration : Migration
+  [Migration(20210214121800)]
+  public class InitialMigration : Migration
+  {
+    public override void Up()
     {
-        public override void Up()
-        {
-            // for great good
-            Execute.Sql("create extension if not exists \"uuid-ossp\";");
+      // for great good
+      Execute.Sql("create extension if not exists \"uuid-ossp\";");
 
-            // TODO unique constraints
-            Create.Schema("stalkmarket");
+      // TODO unique constraints
+      Create.Schema("stalkmarket");
 
-            Create.Table("villager").InSchema("stalkmarket")
-                .WithColumn("id").AsGuid().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
-                .WithColumn("name").AsString();
+      Create.Table("villager").InSchema("stalkmarket")
+          .WithColumn("id").AsGuid().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
+          .WithColumn("name").AsString();
 
-            Create.Table("island").InSchema("stalkmarket")
-                .WithColumn("id").AsGuid().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
-                .WithColumn("name").AsString()
-                .WithColumn("owner_id").AsGuid()
-                .WithColumn("region").AsString()
-                .WithColumn("hemisphere").AsString()
-                .WithColumn("sales_tax").AsInt64().Nullable();
+      Create.Table("island").InSchema("stalkmarket")
+          .WithColumn("id").AsGuid().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
+          .WithColumn("name").AsString()
+          .WithColumn("owner_id").AsGuid()
+          .WithColumn("region").AsString()
+          .WithColumn("hemisphere").AsString()
+          .WithColumn("sales_tax").AsInt64().Nullable();
 
-            Create.Table("stalk").InSchema("stalkmarket")
+      // needs unique constraint by meridian, islandid, datetime
+      Create.Table("stalk").InSchema("stalkmarket")
                 .WithColumn("id").AsGuid().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
                 .WithColumn("date").AsDate()
                 .WithColumn("shop_price").AsString()
@@ -34,8 +35,8 @@ namespace Migrations
                 .WithColumn("island_id").AsGuid()
                 .WithColumn("entered_by").AsGuid();
 
-            // TODO: combine these tables? 
-            Create.Table("stalk_transaction").InSchema("stalkmarket")
+      // needs unique constraint by ? type, villager, timestamp?
+      Create.Table("stalk_transaction").InSchema("stalkmarket")
                 .WithColumn("id").AsGuid().PrimaryKey().WithDefaultValue(SystemMethods.NewGuid)
                 .WithColumn("price").AsString()
                 .WithColumn("on_island").AsGuid()
@@ -44,43 +45,43 @@ namespace Migrations
                 .WithColumn("quantity").AsString()
                 .WithColumn("timestamp").AsDateTime();
 
-            // Ignore activity for now
+      // Ignore activity for now
 
-            Create.ForeignKey("island_owner_id_fk")
-                .FromTable("island").InSchema("stalkmarket")
-                .ForeignColumn("owner_id")
-                .ToTable("villager").InSchema("stalkmarket")
-                .PrimaryColumn("id");
+      Create.ForeignKey("island_owner_id_fk")
+          .FromTable("island").InSchema("stalkmarket")
+          .ForeignColumn("owner_id")
+          .ToTable("villager").InSchema("stalkmarket")
+          .PrimaryColumn("id");
 
-            Create.ForeignKey("island_id_fk")
-                .FromTable("stalk").InSchema("stalkmarket")
-                .ForeignColumn("island_id")
-                .ToTable("island").InSchema("stalkmarket")
-                .PrimaryColumn("id");
+      Create.ForeignKey("island_id_fk")
+          .FromTable("stalk").InSchema("stalkmarket")
+          .ForeignColumn("island_id")
+          .ToTable("island").InSchema("stalkmarket")
+          .PrimaryColumn("id");
 
-            Create.ForeignKey("price_entered_by_fk")
-                .FromTable("stalk").InSchema("stalkmarket")
-                .ForeignColumn("entered_by")
-                .ToTable("villager").InSchema("stalkmarket")
-                .PrimaryColumn("id");
+      Create.ForeignKey("price_entered_by_fk")
+          .FromTable("stalk").InSchema("stalkmarket")
+          .ForeignColumn("entered_by")
+          .ToTable("villager").InSchema("stalkmarket")
+          .PrimaryColumn("id");
 
-            Create.ForeignKey("transaction_on_island_fk")
-                .FromTable("stalk_transaction").InSchema("stalkmarket")
-                .ForeignColumn("on_island")
-                .ToTable("island").InSchema("stalkmarket")
-                .PrimaryColumn("id");
+      Create.ForeignKey("transaction_on_island_fk")
+          .FromTable("stalk_transaction").InSchema("stalkmarket")
+          .ForeignColumn("on_island")
+          .ToTable("island").InSchema("stalkmarket")
+          .PrimaryColumn("id");
 
-            Create.ForeignKey("transaction_villager_id_fk")
-                .FromTable("stalk_transaction").InSchema("stalkmarket")
-                .ForeignColumn("villager_id")
-                .ToTable("villager").InSchema("stalkmarket")
-                .PrimaryColumn("id");
-        }
-
-        public override void Down()
-        {
-            // TODO lol
-            Delete.Table("villager").InSchema("stalkmarket");
-        }
+      Create.ForeignKey("transaction_villager_id_fk")
+          .FromTable("stalk_transaction").InSchema("stalkmarket")
+          .ForeignColumn("villager_id")
+          .ToTable("villager").InSchema("stalkmarket")
+          .PrimaryColumn("id");
     }
+
+    public override void Down()
+    {
+      // TODO lol
+      Delete.Table("villager").InSchema("stalkmarket");
+    }
+  }
 }
--- server/Seeds/DevSeed.cs
+++ server/Seeds/DevSeed.cs
@@ -5,74 +5,111 @@ using Newtonsoft.Json;
 
 namespace Seeds.DevSeed
 {
-    [Profile("Development")]
-    public class DevSeed : Migration
+  [Profile("Development")]
+  public class DevSeed : Migration
+  {
+    public override void Up()
     {
-        public override void Up()
-        {
-            var murgId = Guid.NewGuid();
-            var teatimeId = Guid.NewGuid();
 
-            Insert.IntoTable("villager")
-            .InSchema("stalkmarket")
-            .Row(new
-            {
-                id = murgId,
-                name = "Murg"
-            });
+      var murgId = Guid.NewGuid();
+      var teatimeId = Guid.NewGuid();
 
-            Insert.IntoTable("island")
-            .InSchema("stalkmarket")
-            .Row(new
-            {
-                id = teatimeId,
-                name = "Teatime",
-                owner_id = murgId,
-                hemisphere = "North",
-                region = "NA"
-            });
+      Insert.IntoTable("villager")
+      .InSchema("stalkmarket")
+      .Row(new
+      {
+        id = murgId,
+        name = "Murg"
+      });
 
-            Insert.IntoTable("stalk_transaction")
-            .InSchema("stalkmarket")
-            .Row(new
-            {
-                id = Guid.NewGuid(),
-                price = 92,
-                on_island = teatimeId,
-                type = TransactionType.Buy,
-                villager_id = murgId,
-                quantity = 300,
-                timestamp = DateTime.Parse("2021-02-13T17:51:42+0000")
-            });
+      Insert.IntoTable("island")
+      .InSchema("stalkmarket")
+      .Row(new
+      {
+        id = teatimeId,
+        name = "Teatime",
+        owner_id = murgId,
+        hemisphere = "North",
+        region = "NA"
+      });
 
-            Insert.IntoTable("stalk")
-            .InSchema("stalkmarket")
-            .Row(new
-            {
-                id = Guid.NewGuid(),
-                island_id = teatimeId,
-                meridian = Meridian.AM,
-                shop_price = 102,
-                date = DateTime.Parse("2021-02-15T00:00:00+0000"),
-                entered_by = murgId
-            });
+      Insert.IntoTable("stalk_transaction")
+      .InSchema("stalkmarket")
+      .Row(new
+      {
+        id = Guid.NewGuid(),
+        price = 92,
+        on_island = teatimeId,
+        type = TransactionType.Buy,
+        villager_id = murgId,
+        quantity = 300,
+        timestamp = DateTime.Parse("2021-02-13T17:51:42+0000")
+      });
 
-            Insert.IntoTable("stalk")
-            .InSchema("stalkmarket")
-            .Row(new
-            {
-                id = Guid.NewGuid(),
-                island_id = teatimeId,
-                meridian = Meridian.PM,
-                shop_price = 96,
-                date = DateTime.Parse("2021-02-15T00:00:00+0000"),
-                entered_by = murgId
-            });
-        }
+      Insert.IntoTable("stalk")
+      .InSchema("stalkmarket")
+      .Row(new
+      {
+        id = Guid.NewGuid(),
+        island_id = teatimeId,
+        meridian = Meridian.AM,
+        shop_price = 102,
+        date = DateTime.Parse("2021-02-15T00:00:00+0000"),
+        entered_by = murgId
+      });
 
-        public override void Down()
-        {
-            //empty, not using
-        }
+      Insert.IntoTable("stalk")
+      .InSchema("stalkmarket")
+      .Row(new
+      {
+        id = Guid.NewGuid(),
+        island_id = teatimeId,
+        meridian = Meridian.PM,
+        shop_price = 96,
+        date = DateTime.Parse("2021-02-15T00:00:00+0000"),
+        entered_by = murgId
+      });
+
+      Insert.IntoTable("stalk")
+      .InSchema("stalkmarket")
+      .Row(new
+      {
+        id = Guid.NewGuid(),
+        island_id = teatimeId,
+        meridian = Meridian.AM,
+        shop_price = 60,
+        date = DateTime.Parse("2021-02-16T00:00:00+0000"),
+        entered_by = murgId
+      });
+
+      Insert.IntoTable("stalk")
+      .InSchema("stalkmarket")
+      .Row(new
+      {
+        id = Guid.NewGuid(),
+        island_id = teatimeId,
+        meridian = Meridian.PM,
+        shop_price = 53,
+        date = DateTime.Parse("2021-02-16T00:00:00+0000"),
+        entered_by = murgId
+      });
+
+      Insert.IntoTable("stalk")
+      .InSchema("stalkmarket")
+      .Row(new
+      {
+        id = Guid.NewGuid(),
+        island_id = teatimeId,
+        meridian = Meridian.AM,
+        shop_price = 103,
+        date = DateTime.Parse("2021-02-17T00:00:00+0000"),
+        entered_by = murgId
+      });
     }
+
+    public override void Down()
+    {
+      //empty, not using
+    }
+  }
 }
